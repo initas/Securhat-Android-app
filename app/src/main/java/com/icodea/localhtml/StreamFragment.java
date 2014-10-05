162d5900bc07a23197fd4f98e0810c113f95629e
initas <febri.init@gmail.com>
2014-10-06 01:10:27 +0700

    back button

--- app/src/main/java/com/icodea/localhtml/StreamFragment.java
+++ app/src/main/java/com/icodea/localhtml/StreamFragment.java
@@ -1,36 +1,27 @@
 package com.icodea.localhtml;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.app.Activity;
 import android.graphics.Bitmap;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 import android.app.Fragment;
 
 public class StreamFragment extends Fragment {
-    WebView webView;
+
+    static WebView webView;
     WebSettings webSettings;
     ProgressBar progress;
-    boolean doubleBackToExitPressedOnce;
-
-    public StreamFragment(){}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_stream, container, false);
-        final WebView webView = (WebView)rootView.findViewById(R.id.webview);
-        final ProgressBar progress = (ProgressBar)rootView.findViewById(R.id.pbLoader);
+        webView = (WebView)rootView.findViewById(R.id.webview);
+        progress = (ProgressBar)rootView.findViewById(R.id.pbLoader);
 
         webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -55,14 +46,18 @@ public class StreamFragment extends Fragment {
                 webView.loadUrl("file:///android_asset/my-error-page.html");
             }
         });
+        loadStream();
+        return rootView;
+    }
+    public static void loadStream(){
         webView.clearCache(true);
         webView.loadUrl(Constants.CURHAT_PAGE);
         webView.setBackgroundColor(0);
-
-        return rootView;
     }
-}
-class Constants{
-    public static final String BASE_URL = "file:///android_asset/www/";
-    public static final String CURHAT_PAGE = BASE_URL+"index.html?module=stream";
+    public static boolean canGoBack(){
+        return webView.canGoBack();
+    }
+    public static void goBack(){
+        webView.goBack();
+    }
 }
